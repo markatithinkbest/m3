a108db4c689d7cb953ed0e5cb199d8e892e6ea4f
unknown <mark@ithinkbest.com>
2015-03-06 11:18:11 +0800

    add reg.php

---
+++ reg.php
@@ -0,0 +1,119 @@
+﻿<?php require_once('Connections/members_conn.php'); ?>
+<?php
+// *** Redirect if username exists
+$MM_flag="MM_insert";
+if (isset($_POST[$MM_flag])) {
+  $MM_dupKeyRedirect="index.php";
+  $loginUsername = $_POST['Username'];
+  $LoginRS__query = "SELECT Username FROM members WHERE Username='" . $loginUsername . "'";
+  mysql_select_db($database_members_conn, $members_conn);
+  $LoginRS=mysql_query($LoginRS__query, $members_conn) or die(mysql_error());
+  $loginFoundUser = mysql_num_rows($LoginRS);
+
+  //if there is a row in the database, the username was found - can not add the requested username
+  if($loginFoundUser){
+    $MM_qsChar = "?";
+    //append the username to the redirect page
+    if (substr_count($MM_dupKeyRedirect,"?") >=1) $MM_qsChar = "&";
+    $MM_dupKeyRedirect = $MM_dupKeyRedirect . $MM_qsChar ."requsername=".$loginUsername;
+    header ("Location: $MM_dupKeyRedirect");
+    exit;
+  }
+}
+
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  $theValue = (!get_magic_quotes_gpc()) ? addslashes($theValue) : $theValue;
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? "'" . doubleval($theValue) . "'" : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+
+$editFormAction = $_SERVER['PHP_SELF'];
+if (isset($_SERVER['QUERY_STRING'])) {
+  $editFormAction .= "?" . htmlentities($_SERVER['QUERY_STRING']);
+}
+
+if ((isset($_POST["MM_insert"])) && ($_POST["MM_insert"] == "form1")) {
+  $insertSQL = sprintf("INSERT INTO members (Username, Password, Email, Authcode) VALUES (%s, %s, %s, %s)",
+                       GetSQLValueString($_POST['Username'], "text"),
+                       GetSQLValueString($_POST['Password'], "text"),
+                       GetSQLValueString($_POST['Email'], "text"),
+                       GetSQLValueString($_POST['Authcode'], "text"));
+
+  mysql_select_db($database_members_conn, $members_conn);
+  $Result1 = mysql_query($insertSQL, $members_conn) or die(mysql_error());
+
+  $insertGoTo = "index.php";
+  if (isset($_SERVER['QUERY_STRING'])) {
+    $insertGoTo .= (strpos($insertGoTo, '?')) ? "&" : "?";
+    $insertGoTo .= $_SERVER['QUERY_STRING'];
+  }
+  header(sprintf("Location: %s", $insertGoTo));
+}
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>無標題文件</title>
+</head>
+
+<body>
+<form id="form1" name="form1" method="POST" action="<?php echo $editFormAction; ?>">
+  <table width="277" border="1" align="center">
+    <tr>
+      <td colspan="2"><div align="center">使用者註冊</div></td>
+    </tr>
+    <tr>
+      <td width="86"><div align="right">使用者名稱:</div></td>
+      <td width="175"><label>
+        <div align="center">
+          <input name="Username" type="text" id="Username" />
+        </div>
+      </label></td>
+    </tr>
+    <tr>
+      <td><div align="right">密碼:</div></td>
+      <td><label>
+        <div align="center">
+          <input name="Password" type="password" id="Password" />
+        </div>
+      </label></td>
+    </tr>
+    <tr>
+      <td><div align="right">信箱:</div></td>
+      <td><label>
+        <div align="center">
+          <input name="Email" type="text" id="Email" />
+        </div>
+      </label></td>
+    </tr>
+    <tr>
+      <td colspan="2"><div align="right">
+        <input name="Authcode" type="hidden" id="Authcode" value="Authcode" />
+        登入
+          <input type="submit" name="Submit" value="註冊" />
+      </div></td>
+    </tr>
+  </table>
+  <input type="hidden" name="MM_insert" value="form1">
+</form>
+</body>
+</html>
